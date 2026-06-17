ccc1be8532d795892f77febe9dc5c2ebd2fb9ec5
Qiansen Huang <qiansenhuang@Qiansens-MacBook-Air-5.local>
2026-06-17 10:40:21 -0700

    Add Python profile variables lab

---
+++ profile.py
@@ -0,0 +1,9 @@
+full_name = "Qiansen Huang"
+nickname = "qq"
+age = 17
+used_python_before = False 
+hobbies = ["eating", "playing games", "sleeping", "listening to music"]
+favorites = {"time of day": "night", "game": "RE4", "food": "sushi", "color": "green", "animal": "cat"}
+height = "5ft 4"
+languages_spoken = {"english", "cantonese", "mandarin"}
+print(full_name, nickname, age, used_python_before, hobbies, favorites, languages_spoken, height)
